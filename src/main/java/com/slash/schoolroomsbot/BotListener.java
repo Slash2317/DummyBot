a72f4c4a17e19996e917086c4f69915c1a51f7bf
Slash2317 <marstheredplanet2@gmail.com>
2024-04-24 20:29:14 +0100

    Initial commit

---
+++ src/main/java/com/slash/schoolroomsbot/BotListener.java
@@ -0,0 +1,18 @@
+package com.slash.schoolroomsbot;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class BotListener extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) {
+            return;
+        }
+
+        if (event.getMessage().getContentRaw().equalsIgnoreCase("hi")) {
+            event.getChannel().sendMessage("hello").queue();
+        }
+    }
+}
